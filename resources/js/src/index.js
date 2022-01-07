36233cfb278bb1218a65d315866b6552c10ebde0
RochaMoran <rochamoran1@gmail.com>
2022-01-07 10:55:20 -0600

    REFACTOR: frontend structure

---
+++ resources/js/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import Example from './app'
+
+if (document.getElementById('root')) {
+    ReactDOM.render(<Example />, document.getElementById('root'));
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Facturacion Sistem</title>
+</head>
+<body>
+    <noscript>
+      <p>
+        Bienvenido a la app de naruto
+      </p>
+      <p>
+        La app que estas viendo requiere para su funcionamiento el uso de JavaScript. Si lo has deshabilitado intencionadamente, por favor vuelve a activarlo.
+      </p>
+    </noscript>
+    <div id="root"></div>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+</html>
