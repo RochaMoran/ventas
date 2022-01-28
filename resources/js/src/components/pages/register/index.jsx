b147b41ec31d010adc15058ba92a28688487a36b
Steven Rocha <67056904+RochaMoran@users.noreply.github.com>
2022-01-28 07:51:38 -0600

    REFACTOR: Register and ADD login page

--- resources/js/src/components/pages/register/index.jsx
+++ resources/js/src/components/pages/register/index.jsx
@@ -2,33 +2,40 @@ import React from 'react';
 import ControlInput from '../../controlInput'
 import Button from '../../button'
 import GoogleIcon from '../../../assets/icons/google-icon.png'
+import RegisterImage from '../../../assets/register-image.svg'
+import { useNavigate   } from 'react-router-dom'
 
 export default function Register() {
+  const navigate = useNavigate ();
+
   return (
     <div className='auth'>
-      <form className='auth-form'>
-        <h3 className='auth-title'>Registrarse</h3>
-        <div className='auth-controlInput'>
-          <label>Nombre</label>
-          <ControlInput placeholder="Ingrese su nombre de usuario" />
-        </div>
-        <div className='auth-controlInput'>
-          <label>Correo Eléctronico</label>
-          <ControlInput placeholder="Ingrese su email" />
-        </div>
-        <div className='auth-controlInput'>
-          <label>Contraseña</label>
-          <ControlInput placeholder="Ingrese su contraseña" />
-        </div>
-        <Button className="btn btn-primary" text="Registrarse" />
-        <Button className="btn btn-secondary" text="Iniciar Sesión" />
-        <div className='auth-or__container'>
-          <p>or</p>
-          <button className='btn'>
-            <img src={GoogleIcon} alt="register-or-google" />
-          </button>
-        </div>
-      </form>
+      <div className='auth-container'>
+        <img className='auth-image' src={RegisterImage} alt="register" />
+        <form className='auth-form'>
+          <h3 className='auth-title'>Registrarse</h3>
+          <div className='auth-controlInput'>
+            <label>Nombre</label>
+            <ControlInput placeholder="Ingrese su nombre de usuario" />
+          </div>
+          <div className='auth-controlInput'>
+            <label>Correo Eléctronico</label>
+            <ControlInput placeholder="Ingrese su email" />
+          </div>
+          <div className='auth-controlInput'>
+            <label>Contraseña</label>
+            <ControlInput placeholder="Ingrese su contraseña" />
+          </div>
+          <Button className="btn btn-primary" text="Registrarse" />
+          <Button onClick={() => navigate('/login')} className="btn btn-secondary" text="Iniciar Sesión" />
+          <div className='auth-or__container'>
+            <p>or</p>
+            <button className='btn'>
+              <img src={GoogleIcon} alt="register-or-google" />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
