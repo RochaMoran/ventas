263c407bd5fcdae07ca0d1756faca8e2c471395c
Steven Rocha <67056904+RochaMoran@users.noreply.github.com>
2022-02-17 12:06:28 -0600

    ADD: create facture

--- resources/js/src/components/dashboard/index.jsx
+++ resources/js/src/components/dashboard/index.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import NavBar from "../navbar/index";
 import TopBar from "../topbar/index";
 
-export default function Dashboard({ children }) {
+export default function Dashboard({ main_cls, children }) {
     return (
       <div className="dashboard">
         <TopBar />
         <NavBar />
-        <div className="dashboard-main">
+        <div className={"dashboard-main " + main_cls}>
           <main>
             {children}
           </main>
