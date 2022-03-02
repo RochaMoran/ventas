30e2610267bb6b1c41210b1d3930217d01eb6d72
Steven Rocha <67056904+RochaMoran@users.noreply.github.com>
2022-03-02 07:24:31 -0600

    REFACTOR: Responsive in dashboard

--- resources/js/src/components/barActions/index.jsx
+++ resources/js/src/components/barActions/index.jsx
@@ -3,21 +3,34 @@ import { BiShow, BiEdit } from 'react-icons/bi'
 import { RiDeleteBin6Line } from 'react-icons/ri'
 import { IoMdAddCircleOutline } from 'react-icons/io'
 
-export default function BarActions({create}) {
-  return (
-    <div className='bar-actions'>
-        <button className='button bar-actions__item'>
-            <BiShow /> <span>Mostrar</span>
-        </button>
-        <button className='button bar-actions__item'>
-            <BiEdit /> <span>Editar</span>
-        </button>
-        <button className='button bar-actions__item'>
-            <RiDeleteBin6Line /> <span>Eliminar</span>
-        </button>
-        <button className='button bar-actions__item' onClick={() => create()}>
-            <IoMdAddCircleOutline /> <span>Crear</span>
-        </button>
-    </div>
-  )
+export default function BarActions({action, setAction, create}) {
+    function actionValue (value) {
+        if(action === value) return 'button bar-actions__item bar-action__item-active'
+        else return 'button bar-actions__item'
+    }
+
+    function updateValue (value) {
+        setAction(value)
+        
+        if(value === 'create'){
+            create()
+        }
+    }
+
+    return (
+        <div className='bar-actions'>
+            <button className={actionValue("show")} onClick={() => updateValue("show")}>
+                <BiShow /> <span>Mostrar</span>
+            </button>
+            <button className={actionValue("update")} onClick={() => updateValue("update")}>
+                <BiEdit /> <span>Editar</span>
+            </button>
+            <button className={actionValue("delete")} onClick={() => updateValue("delete")}>
+                <RiDeleteBin6Line /> <span>Eliminar</span>
+            </button>
+            <button className={actionValue("create")} onClick={() => updateValue("create")}>
+                <IoMdAddCircleOutline /> <span>Crear</span>
+            </button>
+        </div>
+    )
 }
